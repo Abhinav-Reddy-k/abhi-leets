f27001b28c23fa195690d41bdce4f7afa6a116ca
Abhinav Reddy <63109580+Abhinav-Reddy-k@users.noreply.github.com>
2022-05-09 19:29:31 +0530

    Time: 106 ms (18.94%), Space: 18.6 MB (17.99%) - LeetHub

--- 501-find-mode-in-binary-search-tree/501-find-mode-in-binary-search-tree.py
+++ 501-find-mode-in-binary-search-tree/501-find-mode-in-binary-search-tree.py
@@ -6,15 +6,17 @@
 #         self.right = right
 class Solution:
     def findMode(self, root: Optional[TreeNode]) -> List[int]:
-        c = defaultdict(int)
+        dic = defaultdict(int)
         
         q = deque([root])
         while q:
             n = q.popleft()
             if not n:
                 continue
-            c[n.val] += 1
+            dic[n.val] += 1
             q.append(n.left)
             q.append(n.right)
         
-        return [k for k, v in Counter(c).items() if v == max(Counter(c).values())]
+        max_freq = max(dic.values())
+        return [k for k, v in dic.items() if v == max_freq]
+        
