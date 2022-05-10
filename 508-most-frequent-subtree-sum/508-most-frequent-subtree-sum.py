5b7b2cf53d836ee59d75d84b45d296df2a25cc1e
Abhinav Reddy <63109580+Abhinav-Reddy-k@users.noreply.github.com>
2022-05-10 17:21:43 +0530

    Time: 1624 ms (5.04%), Space: 17.4 MB (76.51%) - LeetHub

---
+++ 508-most-frequent-subtree-sum/508-most-frequent-subtree-sum.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findFrequentTreeSum(self, root: Optional[TreeNode]) -> List[int]:
+        freq = defaultdict(int)
+        
+        def dfs(node):
+            if not node:
+                return 0
+            
+            cur = node.val + dfs(node.left) + dfs(node.right)
+            freq[cur] += 1
+            return cur
+        
+        dfs(root)
+        return [key for key, value in freq.items() if value == max(freq.values())]
